676de64b4c3bb8ab16f37aaf64f6aacb9b3845b7
Andrés Esguerra <kaosterra@gmail.com>
2014-10-17 08:34:13 -0500

    Upgrade
    Actualización a versión más reciente de generador

--- Marketplace/MarketplaceProject.web/src/main/webapp/src/component/_cartMasterComponent.js
+++ Marketplace/MarketplaceProject.web/src/main/webapp/src/component/_cartMasterComponent.js
@@ -1,69 +1,63 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-define(['controller/tabController', 'component/cartComponent', 'component/itemComponent', 'controller/selectionController', 'model/cacheModel', 'model/cartMasterModel', 'component/_CRUDComponent'], function(TabController, CartComponent, itemComponent){
-	App.Component._CartMasterComponent = App.Component.BasicComponent.extend({
-		initialize: function () {
-			var self = this;
-			this.configuration = App.Utils.loadComponentConfiguration('cartMaster');
-			App.Model.CartMasterModel.prototype.urlRoot = this.configuration.context;
-			this.componentId = App.Utils.randomInteger();
-			
-			this.masterComponent = new CartComponent();
-			this.masterComponent.initialize();
-			
-			this.childComponents = [];
+define(['controller/selectionController', 'model/cacheModel', 'model/cartMasterModel', 'component/_CRUDComponent', 'controller/tabController', 'component/cartComponent',
+ 'component/itemComponent'],
+ function(SelectionController, CacheModel, CartMasterModel, CRUDComponent, TabController, CartComponent,
+ itemComponent) {
+    App.Component._CartMasterComponent = App.Component.BasicComponent.extend({
+        initialize: function() {
+            var self = this;
+            this.configuration = App.Utils.loadComponentConfiguration('cartMaster');
+            App.Model.CartMasterModel.prototype.urlRoot = this.configuration.context;
+            this.componentId = App.Utils.randomInteger();
+            
+            this.masterComponent = new CartComponent();
+            this.masterComponent.initialize();
+            
+            this.childComponents = [];
 			
 			this.initializeChildComponents();
-			
-			Backbone.on(this.masterComponent.componentId + '-post-cart-create', function (params) {
-				self.renderChilds(params);
-			});
-			Backbone.on(this.masterComponent.componentId + '-post-cart-edit', function (params) {
-				self.renderChilds(params);
-			});
-			Backbone.on(this.masterComponent.componentId + '-pre-cart-list', function () {
-				self.hideChilds();
-			});
-			Backbone.on('cart-master-model-error', function (error) {
-				Backbone.trigger(this.masterComponent.componentId + '-' + 'error', {event: 'cart-master-save', view: self, message: error});
-			});
-			Backbone.on(this.masterComponent.componentId + '-instead-cart-save', function (params) {
-				self.model.set('cartEntity', params.model);
-				if (params.model) {
-					self.model.set('id', params.model.id);
-				} else {
-					self.model.unset('id');
+            
+            Backbone.on(this.masterComponent.componentId + '-post-cart-create', function(params) {
+                self.renderChilds(params);
+            });
+            Backbone.on(this.masterComponent.componentId + '-post-cart-edit', function(params) {
+                self.renderChilds(params);
+            });
+            Backbone.on(this.masterComponent.componentId + '-pre-cart-list', function() {
+                self.hideChilds();
+            });
+            Backbone.on('cart-master-model-error', function(error) {
+                Backbone.trigger(uComponent.componentId + '-' + 'error', {event: 'cart-master-save', view: self, message: error});
+            });
+            Backbone.on(this.masterComponent.componentId + '-instead-cart-save', function(params) {
+                self.model.set('cartEntity', params.model);
+                if (params.model) {
+                    self.model.set('id', params.model.id);
+                } else {
+                    self.model.unset('id');
+                }
+
+				App.Utils.fillCacheList(
+					'item',
+					self.model,
+					self.itemComponent.getDeletedRecords(),
+					self.itemComponent.getUpdatedRecords(),
+					self.itemComponent.getCreatedRecords()
+				);
+
+                self.model.save({}, {
+                    success: function() {
+                        Backbone.trigger(self.masterComponent.componentId + '-' + 'post-cart-save', {view: self, model : self.model});
+                    },
+                    error: function(error) {
+                        Backbone.trigger(self.componentId + '-' + 'error', {event: 'cart-master-save', view: self, error: error});
+                    }
+                });
+			    if (this.postInit) {
+					this.postInit();
 				}
-				
-				for (var idx in self.childComponents) {
-					App.Utils.fillCacheList(
-						self.childComponents[idx].name,
-						self.model,
-						self.childComponents[idx].getDeletedRecords(),
-						self.childComponents[idx].getUpdatedRecords(),
-						self.childComponents[idx].getCreatedRecords()
-					);
-				}
-				
-				self.model.save({}, {
-					success: function () {
-						Backbone.trigger(self.masterComponent.componentId + '-' + 'post-cart-save', {view: self, model: self.model});
-					},
-					error: function (error) {
-						Backbone.trigger(self.componentId + '-' + 'error', {event: 'cart-master-save', view: self, error: error});
-					}
-				});
-			});
-			if (this.postInit) {
-				this.postInit();
-			}
-		},
-		render: function(domElementId){
+            });
+        },
+        render: function(domElementId){
 			if (domElementId) {
 				var rootElementId = $("#"+domElementId);
 				this.masterElement = this.componentId + "-master";
@@ -75,51 +69,51 @@ define(['controller/tabController', 'component/cartComponent', 'component/itemCo
 			this.masterComponent.render(this.masterElement);
 		},
 		initializeChildComponents: function () {
-			this.tabModel = new App.Model.TabModel({tabs: [{label: "Item", name: "item", enable: true}]});
+			this.tabModel = new App.Model.TabModel({tabs: [
+                {label: "Item", name: "item", enable: true}
+			]});
 			this.tabs = new TabController({model: this.tabModel});
-			
+
 			this.itemComponent = new itemComponent();
-			this.itemComponent.initialize(
-					{
-						cache: {
-							data: [],
-							mode: "memory"
-						},
-						pagination: false
-					});
-			Backbone.on(this.itemComponent.componentId + '-post-item-create', function (params) {
-				params.view.currentModel.setCacheList(params.view.currentList);
-			});
-			this.configToolbar(this.itemComponent, true);
+            this.itemComponent.initialize({cache: {data: [], mode: "memory"},pagination: false});
 			this.childComponents.push(this.itemComponent);
+
+            var self = this;
+            
+            this.configToolbar(this.itemComponent,true);
+            Backbone.on(self.itemComponent.componentId + '-post-item-create', function(params) {
+                params.view.currentModel.setCacheList(params.view.currentList);
+            });
+            
 		},
-		renderChilds: function (params) {
-			var self = this;
-			
-			var options = {
-				success: function () {
-					self.tabs.render(self.tabsElement);
-					for (var idx in self.childComponents) {
-						var name = self.childComponents[idx].name;
-						self.childComponents[idx].clearCache();
-						self.childComponents[idx].setRecords(self.model.get('list'+name));
-						self.childComponents[idx].render(self.tabs.getTabHtmlId(name));
-					}
-					$('#'+self.tabsElement).show();
-				},
-				error: function (error) {
-					Backbone.trigger(self.componentId + '-' + 'error', {event: 'cart-edit', view: self, id: id, data: data, error: error});
-				}
-			};
-			if (params.id) {
-				self.model = new App.Model.CartMasterModel({id: params.id});
-				self.model.fetch(options);
-			} else {
-				self.model = new App.Model.CartMasterModel();
-				options.success();
-			}
-		},
-		showMaster: function (flag) {
+        renderChilds: function(params) {
+            var self = this;
+            
+            var options = {
+                success: function() {
+                	self.tabs.render(self.tabsElement);
+
+					self.itemComponent.clearCache();
+					self.itemComponent.setRecords(self.model.get('listitem'));
+					self.itemComponent.render(self.tabs.getTabHtmlId('item'));
+
+                    $('#'+self.tabsElement).show();
+                },
+                error: function() {
+                    Backbone.trigger(self.componentId + '-' + 'error', {event: 'cart-edit', view: self, id: id, data: data, error: error});
+                }
+            };
+            if (params.id) {
+                self.model = new App.Model.CartMasterModel({id: params.id});
+                self.model.fetch(options);
+            } else {
+                self.model = new App.Model.CartMasterModel();
+                options.success();
+            }
+
+
+        },
+        showMaster: function (flag) {
 			if (typeof (flag) === "boolean") {
 				if (flag) {
 					$("#"+this.masterElement).show();
@@ -128,11 +122,11 @@ define(['controller/tabController', 'component/cartComponent', 'component/itemCo
 				}
 			}
 		},
-		hideChilds: function () {
-			$("#"+this.tabsElement).hide();
-		},
-		configToolbar: function (component, composite) {
-			component.removeGlobalAction('refresh');
+        hideChilds: function() {
+            $("#"+this.tabsElement).hide();
+        },
+		configToolbar: function(component, composite) {
+		    component.removeGlobalAction('refresh');
 			component.removeGlobalAction('print');
 			component.removeGlobalAction('search');
 			if (!composite) {
@@ -148,8 +142,8 @@ define(['controller/tabController', 'component/cartComponent', 'component/itemCo
 					Backbone.trigger(component.componentId + '-toolbar-add');
 				});
 			}
-		},
-		getChilds: function(name){
+        },
+        getChilds: function(name){
 			for (var idx in this.childComponents) {
 				if (this.childComponents[idx].name === name) {
 					return this.childComponents[idx].getRecords();
@@ -173,6 +167,7 @@ define(['controller/tabController', 'component/cartComponent', 'component/itemCo
 				}
 			}
 		}
-	});
-	return App.Component._CartMasterComponent;
+    });
+
+    return App.Component._CartMasterComponent;
 });
--- Marketplace/MarketplaceProject.web/src/main/webapp/src/component/cartMasterComponent.js
+++ Marketplace/MarketplaceProject.web/src/main/webapp/src/component/cartMasterComponent.js
@@ -1,9 +1,8 @@
-define(['component/_cartMasterComponent'], function (_CartMasterComponent) {
-	App.Component.CartMasterComponent = _CartMasterComponent.extend({
+define(['component/_cartMasterComponent'],function(_CartMasterComponent) {
+    App.Component.CartMasterComponent = _CartMasterComponent.extend({
 		postInit: function(){
-			//Escribir aquí las instrucciones que desea ejecutar al inicializar el componente
+			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
 		},
-		
 		addItems: function (params) {
 			var list = this.itemComponent.getRecords();
 			for (var idx in params) {
@@ -17,6 +16,7 @@ define(['component/_cartMasterComponent'], function (_CartMasterComponent) {
 				}
 			}
 		}
-	});
-	return App.Component.CartMasterComponent;
+    });
+
+    return App.Component.CartMasterComponent;
 });
